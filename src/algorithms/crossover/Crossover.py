b68d68fa8b144b9c73ade74c64d9887a6f6b7530
Damian Kluczyński <damiankluczynski00@gmail.com>
2024-04-07 00:32:15 +0200

    three point crossover

--- src/algorithms/crossover/Crossover.py
+++ src/algorithms/crossover/Crossover.py
@@ -9,8 +9,12 @@ class Crossover:
     def single_point_crossover(self, chromosome_1, chromosome_2):
         if random.random() < self.crossover_prob:
             point = random.randint(1, len(chromosome_1) - 2)
-            child_1 = np.concatenate((chromosome_1[:point], chromosome_2[point:]))
-            child_2 = np.concatenate((chromosome_2[:point], chromosome_1[point:]))
+            child_1 = np.concatenate((
+                chromosome_1[:point],
+                chromosome_2[point:]))
+            child_2 = np.concatenate((
+                chromosome_2[:point],
+                chromosome_1[point:]))
             return child_1, child_2
         return chromosome_1, chromosome_2
 
@@ -18,7 +22,29 @@ class Crossover:
         if random.random() < self.crossover_prob:
             point_1 = random.randint(1, len(chromosome_1) - 3)
             point_2 = random.randint(point_1 + 1, len(chromosome_1) - 2)
-            child_1 = np.concatenate((chromosome_1[:point_1], chromosome_2[point_1:point_2], chromosome_1[point_2:]))
-            child_2 = np.concatenate((chromosome_2[:point_1], chromosome_1[point_1:point_2], chromosome_2[point_2:]))
+            child_1 = np.concatenate((
+                chromosome_1[:point_1],
+                chromosome_2[point_1:point_2],
+                chromosome_1[point_2:]))
+            child_2 = np.concatenate((
+                chromosome_2[:point_1],
+                chromosome_1[point_1:point_2],
+                chromosome_2[point_2:]))
+            return child_1, child_2
+        return chromosome_1, chromosome_2
+
+    def three_point_crossover(self, chromosome_1, chromosome_2):
+        if random.random() < self.crossover_prob:
+            points = sorted(random.sample(range(1, len(chromosome_1) - 1), 3))
+            child_1 = np.concatenate((
+                chromosome_1[:points[0]],
+                chromosome_2[points[0]:points[1]],
+                chromosome_1[points[1]:points[2]],
+                chromosome_2[points[2]:]))
+            child_2 = np.concatenate((
+                chromosome_2[:points[0]],
+                chromosome_1[points[0]:points[1]],
+                chromosome_2[points[1]:points[2]],
+                chromosome_1[points[2]:]))
             return child_1, child_2
         return chromosome_1, chromosome_2
