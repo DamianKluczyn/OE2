56e6f1a25f7d7c822fd6cea2fc46185e19e9ff2e
Damian Kluczyński <damiankluczynski00@gmail.com>
2024-04-07 23:16:41 +0200

    added work list

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from utilities import generating_files
 from optimization import optimization
 from population import Chromosome
 
+#TODO klasa populacja, klasa osobnik, klasa fitness function
+
 if __name__ == '__main__':
     config = config.Config()
     chromosome = Chromosome.Chromosome(
@@ -14,3 +16,24 @@ if __name__ == '__main__':
         config.get_param("algorithm_parameters.binary_precision")
     )
 
+
+"""
+Generowanie populacji
+(klasa populacjia, chromosom, osobnik, fitness function(?))
+
+Selekcja (np. z 20 osobnikow 10 zostanie wybranych)
+
+Krzyżowanie - dopóki populacja nie stanie się tak duża jak pierwotna,
+losowanie z powtórzeniami
+(jak używamy strategii elitarnej to osobniki wybrane przechodzą bez następnych kroków,
+20-początkowa, 2-elitarna to 18 trzeba dostać z krzyżowań)
+
+Mutacja
+
+Inwersja
+
+Zakończenie 1 epoki iteracji - 18 osobników z krzyżowania,mutacji,inwersji + 2 z elitarnej
+
+Powtórzenie całości przez daną liczbę epok
+
+"""
