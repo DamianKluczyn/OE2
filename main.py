4d44cf2bc1fbc8fbb9c207020106d10ce6a209f7
Damian Kluczyński <damiankluczynski00@gmail.com>
2024-04-09 01:08:09 +0200

    fixed gui work

--- main.py
+++ main.py
@@ -1,27 +1,52 @@
-# from algorithms.crossover import crossover
-# from algorithms.mutation import mutation
-# from algorithms.selection import selection
-# from configuration import config
-# from utilities import generating_files
-# from optimization import optimization
-# from population import Chromosome
+from algorithms.crossover import crossover
+from algorithms.mutation import mutation
+from algorithms.selection import selection
+from configuration import config
+from utilities import generating_files
+from optimization import optimization
+from population import chromosome
 from src.configuration.config import Config
 from src.population.population import Population
+from gui import gui
 
 import time
 
 
 def main_function():
+    config = Config()
+
+    start_range = config.get_param('algorithm_parameters.start_range_a', -10)
+    end_range = config.get_param('algorithm_parameters.end_range_b', 10)
+    population_size = config.get_param('algorithm_parameters.population_size', 20)
+    binary_precision = config.get_param('algorithm_parameters.binary_precision', 6)
+    number_of_variables = config.get_param('algorithm_parameters.number_of_variables', 10)
+    number_of_epochs = config.get_param('algorithm_parameters.number_of_epochs', 50)
+
+    population = Population(population_size, number_of_variables, (start_range, end_range), binary_precision)
+    print(f"Initial population: {population}")
+
     start_time = time.time()
-    time.sleep(5)
+
+    for epoch in range(number_of_epochs):
+        # selection = GeneticSelection(...)
+        # offspring = selection.select(population)
+        # crossover = Crossover(...)
+        # mutated_offspring = Mutation(...)
+        # population.update(mutated_offspring)
+
+        print(f"Epoch {epoch + 1}/{number_of_epochs} completed.")
+
     end_time = time.time()
-    return end_time - start_time
+    exec_time = end_time - start_time
+    print(f"Algorithm finished in {exec_time} seconds.")
 
 
 if __name__ == '__main__':
-    config = Config()
-    population = Population(3, 3, (-10, 10), 6)
-    print(population)
+    # config = Config()
+    # population = Population(3, 3, (-10, 10), 6)
+    # print(population)
+    app = gui.GUIClass()
+    app.mainloop()
 
 """
 Generowanie populacji
--- src/gui/gui.py
+++ src/gui/gui.py
@@ -2,11 +2,9 @@ import tkinter.messagebox
 from tkinter import *
 from tkinter import ttk
 import re
-from configuration import config
-
+from configuration.config import Config
 from IPython.terminal.pt_inputhooks import tk
 
-from configuration.config import Config
 from main import main_function
 
 
@@ -58,68 +56,74 @@ class GUIClass(Tk):
                                       validate="focusout", validatecommand=vcmd, invalidcommand=ivcmd)
         self.epochs_entry.grid(column=1, row=4, sticky="N E", padx=5, pady=5)
 
-        ttk.Label(mainframe, text="Best and tournament chromosome amount:").grid(column=0, row=5, sticky=W)
+        ttk.Label(mainframe, text="Number of chromosomes in specimen:").grid(column=0, row=5, sticky=W)
+        self.number_of_variables = IntVar()
+        self.number_of_variables_entry = ttk.Entry(mainframe, width=15, textvariable=self.number_of_variables,
+                                      validate="focusout", validatecommand=vcmd, invalidcommand=ivcmd)
+        self.number_of_variables_entry.grid(column=1, row=5, sticky="N E", padx=5, pady=5)
+
+        ttk.Label(mainframe, text="Best and tournament chromosome amount:").grid(column=0, row=6, sticky=W)
         self.best_chromosome = IntVar()
         self.best_chromosome_entry = ttk.Entry(mainframe, width=15, textvariable=self.best_chromosome,
                                                validate="focusout", validatecommand=vcmd, invalidcommand=ivcmd)
-        self.best_chromosome_entry.grid(column=1, row=5, sticky="N E", padx=5, pady=5)
+        self.best_chromosome_entry.grid(column=1, row=6, sticky="N E", padx=5, pady=5)
 
-        ttk.Label(mainframe, text="Elite Strategy amount:").grid(column=0, row=6, sticky=W)
+        ttk.Label(mainframe, text="Elite Strategy amount:").grid(column=0, row=7, sticky=W)
         self.elite_strategy = IntVar()
         self.elite_strategy_entry = ttk.Entry(mainframe, width=15, textvariable=self.elite_strategy,
                                               validate="focusout", validatecommand=vcmd, invalidcommand=ivcmd)
-        self.elite_strategy_entry.grid(column=1, row=6, sticky="N E", padx=5, pady=5)
+        self.elite_strategy_entry.grid(column=1, row=7, sticky="N E", padx=5, pady=5)
 
-        ttk.Label(mainframe, text="Cross probability:").grid(column=0, row=7, sticky=W)
+        ttk.Label(mainframe, text="Cross probability:").grid(column=0, row=8, sticky=W)
         self.crossover_prob = DoubleVar()
         self.crossover_prob_entry = ttk.Entry(mainframe, width=15, textvariable=self.crossover_prob,
                                               validate="focusout", validatecommand=vcmd, invalidcommand=ivcmd)
-        self.crossover_prob_entry.grid(column=1, row=7, sticky="N E", padx=5, pady=5)
+        self.crossover_prob_entry.grid(column=1, row=8, sticky="N E", padx=5, pady=5)
 
-        ttk.Label(mainframe, text="Mutation probability:").grid(column=0, row=8, sticky=W)
+        ttk.Label(mainframe, text="Mutation probability:").grid(column=0, row=9, sticky=W)
         self.mutation_prob = DoubleVar()
         self.mutation_prob_entry = ttk.Entry(mainframe, width=15, textvariable=self.mutation_prob,
                                              validate="focusout", validatecommand=vcmd, invalidcommand=ivcmd)
-        self.mutation_prob_entry.grid(column=1, row=8, sticky="N E", padx=5, pady=5)
+        self.mutation_prob_entry.grid(column=1, row=9, sticky="N E", padx=5, pady=5)
 
-        ttk.Label(mainframe, text="Inversion probability:").grid(column=0, row=9, sticky=W)
+        ttk.Label(mainframe, text="Inversion probability:").grid(column=0, row=10, sticky=W)
         self.inversion_prob = DoubleVar()
         self.inversion_prob_entry = ttk.Entry(mainframe, width=15, textvariable=self.inversion_prob,
                                               validate="focusout", validatecommand=vcmd, invalidcommand=ivcmd)
-        self.inversion_prob_entry.grid(column=1, row=9, sticky="N E", padx=5, pady=5)
+        self.inversion_prob_entry.grid(column=1, row=10, sticky="N E", padx=5, pady=5)
 
         ttk.Label(mainframe, text="Selection method:").grid(column=0, row=11, sticky=W)
         self.selection_method = StringVar()
         self.selection_method_combo = ttk.Combobox(mainframe, textvariable=self.selection_method)
         self.selection_method_combo['values'] = ("Roulette Wheel", "Tournament", "Select best")
-        self.selection_method_combo.grid(column=0, row=12, sticky="N W", padx=5, pady=5)
+        self.selection_method_combo.grid(column=1, row=11, sticky="N W", padx=5, pady=5)
 
-        ttk.Label(mainframe, text="Crossover method:").grid(column=0, row=13, sticky=W)
+        ttk.Label(mainframe, text="Crossover method:").grid(column=0, row=12, sticky=W)
         self.crossover_method = StringVar()
         self.crossover_prob_combo = ttk.Combobox(mainframe, textvariable=self.crossover_method)
         self.crossover_prob_combo['values'] = ("Single Point", "Two Point", "Three Point", "Uniform", "Discrete",
                                                "Self", "Binary", "Linkage Evolution")
-        self.crossover_prob_combo.grid(column=0, row=14, sticky="N W", padx=5, pady=5)
+        self.crossover_prob_combo.grid(column=1, row=12, sticky="N W", padx=5, pady=5)
 
-        ttk.Label(mainframe, text="Mutation method:").grid(column=0, row=15, sticky=W)
+        ttk.Label(mainframe, text="Mutation method:").grid(column=0, row=13, sticky=W)
         self.mutation_method = StringVar()
         self.mutation_method_combo = ttk.Combobox(mainframe, textvariable=self.mutation_method)
         self.mutation_method_combo['values'] = ("Boundary", "One Point", "Two Point")
-        self.mutation_method_combo.grid(column=0, row=16, sticky="N W", padx=5, pady=5)
+        self.mutation_method_combo.grid(column=1, row=13, sticky="N W", padx=5, pady=5)
 
         self.use_elite_strategy_var = BooleanVar()
         self.use_elite_strategy_checkbutton = ttk.Checkbutton(mainframe, text="Elite Strategy", variable=self.use_elite_strategy_var)
-        self.use_elite_strategy_checkbutton.grid(column=0, row=17, sticky=W, padx=5, pady=5)
+        self.use_elite_strategy_checkbutton.grid(column=0, row=14, sticky=W, padx=5, pady=5)
 
         self.maximization = BooleanVar()
         self.maximization_check = ttk.Checkbutton(mainframe, text="Maximization", variable=self.maximization)
-        self.maximization_check.grid(column=0, row=18, sticky=W, padx=5, pady=5)
+        self.maximization_check.grid(column=0, row=15, sticky=W, padx=5, pady=5)
 
         self.start_button = ttk.Button(mainframe, text="Start", padding="10 10 10 10", command=self.get_values)
-        self.start_button.grid(row=18, padx=5, pady=5, sticky="N E S W")
+        self.start_button.grid(row=16, padx=5, pady=5, sticky="N E S W")
 
         self.error_message = ttk.Label(mainframe, foreground="red")
-        self.error_message.grid(row=19, padx=5, pady=5, sticky="N E S W")
+        self.error_message.grid(row=17, padx=5, pady=5, sticky="N E S W")
 
     def validate_entry_data(self, value):
         pattern = r'[+]?([0-9]*[.])?[0-9]+'
@@ -141,18 +145,22 @@ class GUIClass(Tk):
         config.set_param('algorithm_parameters.population_size', self.pop_size.get())
         config.set_param('algorithm_parameters.binary_precision', self.precision_bits.get())
         config.set_param('algorithm_parameters.number_of_epochs', self.epochs.get())
+        config.set_param('algorithm_parameters.number_of_variables', self.number_of_variables.get())
         config.set_param('algorithm_parameters.selection_method', self.selection_method.get().lower().replace(" ", "_"))
+        config.set_param('algorithm_parameters.selection_parameters.tournament_size', self.best_chromosome.get())
         config.set_param('algorithm_parameters.crossover_probability', self.crossover_prob.get())
         config.set_param('algorithm_parameters.mutation_probability', self.mutation_prob.get())
         config.set_param('algorithm_parameters.inversion_probability', self.inversion_prob.get())
         config.set_param('algorithm_parameters.elite_strategy.use_elite_strategy', self.use_elite_strategy_var.get())
         config.set_param('algorithm_parameters.elite_strategy.elite_count', self.elite_strategy.get())
+        config.set_param('algorithm_parameters.crossover_method', self.crossover_method.get())
+        config.set_param('algorithm_parameters.maximization', self.maximization.get())
 
         exec_time = main_function()
-        self.info_box = tkinter.messagebox.showinfo(title='Obliczenia ewolucyjne - Projekt 2',
-                                                    message=f'Execution time: {round(exec_time, 4)}s')
+        # self.info_box = tkinter.messagebox.showinfo(title='Obliczenia ewolucyjne - Projekt 2',
+        #                                             message=f'Execution time: {round(exec_time, 4)}s')
 
 
-if __name__ == "__main__":
-    app = GUIClass()
-    app.mainloop()
+# if __name__ == "__main__":
+#     app = GUIClass()
+#     app.mainloop()
