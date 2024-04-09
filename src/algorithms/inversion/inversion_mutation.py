b21d4d006592de7a4aada1ac5dcd539e8e402826
Kamil <kamil.deren1@gmail.com>
2024-04-09 19:13:54 +0200

    changed inversion

--- src/algorithms/inversion/inversion_mutation.py
+++ src/algorithms/inversion/inversion_mutation.py
@@ -5,12 +5,19 @@ class InversionMutation:
     def __init__(self, mutation_rate):
         self.mutation_rate = mutation_rate
 
-    def inversion_mutation(self, chromosome):
-        mutated_chromosome = chromosome[:]
+    def inversion_mutation(self, specimen1):
+        mutated_specimen = []
 
-        if random.random() < self.mutation_rate:
-            start = random.randint(0, len(chromosome) - 1)
-            end = random.randint(start, len(chromosome))
-            mutated_chromosome[start:end] = reversed(chromosome[start:end])
+        for i in range(len(specimen1.specimen)):
+            chromosome = specimen1.specimen[i].chromosome
 
-        return mutated_chromosome
+            mutated_chromosome1 = chromosome[:]
+
+            if random.random() < self.mutation_rate:
+                start = random.randint(0, len(chromosome) - 1)
+                end = random.randint(start, len(chromosome))
+                mutated_chromosome1[start:end] = reversed(chromosome[start:end])
+
+            mutated_specimen.append(mutated_chromosome1)
+
+        return mutated_specimen
